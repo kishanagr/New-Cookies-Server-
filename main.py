60a64fb484c566ed89e4ddf3140b5282018b2f5d
kishanagr <agraharisanskar65@gmail.com>
2026-02-06 12:37:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-from flask import Flask, request, render_template_string, redirect, url_for, session
+import streamlit as st
 import requests
-from threading import Thread, Event
+import threading
 import time
-import os
 import random
 import string
-
-app = Flask(__name__)
-app.secret_key = 'secret_key_for_session_management'  # Replace with a strong secret key
-
-USERNAME = "ALONE_SANSKARI_KING"
-PASSWORD = "SANSKARI_FT <3 (Y)"
-
-headers = {
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
-    'referer': 'www.google.com'
-}
-
-stop_events = {}
-threads = {}
+from datetime import datetime
 
 # Page configuration
 st.set_page_config(
